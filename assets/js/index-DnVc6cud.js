f47055beb874a2458052318c2ec911d9c5eaa43a
kstretiakova <146082340+kstretiakova@users.noreply.github.com>
2025-01-22 02:37:37 +0000

    Deploying to gh-pages from @ kstretiakova/ypojyachts@e35018cfaf367e234274edabb25a212b00747add 🚀

--- assets/js/index-DnVc6cud.js
+++ assets/js/index-DnVc6cud.js
@@ -64,4 +64,4 @@
           to { opacity: 1; }
       }
   `,document.head.appendChild(r),document.getElementById("closeCrewModalButton").addEventListener("click",function(){document.body.removeChild(a)})}function w(a){return/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(a)}
-//# sourceMappingURL=index-Cuh1vN0r.js.map
+//# sourceMappingURL=index-DnVc6cud.js.map
